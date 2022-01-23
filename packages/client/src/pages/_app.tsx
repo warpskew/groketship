b2920f059c3a671f535825fa401ad8ae75d4beb1
Oscar Fuentes <oscar_fuentes@alumni.brown.edu>
2022-01-23 10:33:59 -0800

    fix: polyfill setImmediate() API

---
+++ packages/client/src/pages/_app.tsx
@@ -0,0 +1,24 @@
+/**
+ * @file Defines the custom application component.
+ */
+
+import 'setimmediate';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+
+export default function Application({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        {/* TODO: Do we need these two (2) tags in production? */}
+        <meta charSet="utf-8" key="char-set" />
+        <meta
+          content="width=device-width, initial-scale=1, viewport-fit=cover"
+          key="viewport"
+          name="viewport"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
